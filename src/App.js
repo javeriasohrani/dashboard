62e77e465fe29044423df023d421d0700e2dcfba
usmanalinarsun <84831586+usmanalinarsun@users.noreply.github.com>
2021-09-16 14:18:03 +0500

    create header  component and inmport inside app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Header from "./Components/Header";
 import Test from "./Components/test";
 
 function App() {
   return (
     <div className="App">
-        <Test />
+      <Header />
+      <Test />
     </div>
   );
 }
