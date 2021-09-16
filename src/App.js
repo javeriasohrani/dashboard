c7097bfcb48bac81ceb8cfa60665c6e3dfee5560
javeriasohrani <83175699+javeriasohrani@users.noreply.github.com>
2021-09-16 02:26:54 -0700

    asd

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import "./App.css";
-import Header from "./Components/Header";
-import Test from "./Components/test";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Test />
+
     </div>
   );
 }
