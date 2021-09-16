d6831b71ee3f15010b54b500b809cecf2e6f073b
javeriasohrani <83175699+javeriasohrani@users.noreply.github.com>
2021-09-16 01:53:21 -0700

    update app.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-        <h1>App js updated and again I have updated the code i app.js file</h1>
+        <h1>App js updated I am sidebar menu branch</h1>
     </div>
   );
 }
