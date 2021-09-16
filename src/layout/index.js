0904bea72b3d8bd9a0484a96291e3a86bb8a5187
javeriasohrani <83175699+javeriasohrani@users.noreply.github.com>
2021-09-16 01:35:51 -0700

    updated code

---
+++ src/layout/index.js
@@ -0,0 +1,14 @@
+// core
+import React from "react";
+
+// custom components
+import "./layout.css";
+
+
+export default function Layout(props) {
+  return (
+    <div className="layout">
+    
+    </div>
+  );
+}
