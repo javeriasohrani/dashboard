b330afbda4c2397fc13f83cc769b936a572852fd
javeriasohrani <83175699+javeriasohrani@users.noreply.github.com>
2021-09-16 03:40:32 -0700

    update sidebar

---
+++ src/Components/Sidebar/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Sidebar() {
+  return (
+    <div className="SidebarMenu-wrap">
+      <div className="Sidebar">
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+
+        <ul>
+          <li></li>
+        </ul>
+      </div>
+    </div>
+  );
+}
