7228b006b7fbdf6c214c32fd815c4025163d8204
javeriasohrani <83175699+javeriasohrani@users.noreply.github.com>
2021-09-16 04:11:15 -0700

    add svg in sidebar

--- src/Components/Sidebar/index.js
+++ src/Components/Sidebar/index.js
@@ -1,32 +1,39 @@
 import React from "react";
+import "../Sidebar.css";
+import SvgDashboard from "../../svg/Dashboard";
+import SvgHelp from "../../svg/Help";
+import SvgMessages from "../../svg/Messages";
+import SvgPages from "../../svg/Pages";
+import SvgRoles from "../../svg/Roles";
+import SvgSettings from "../../svg/Settings";
 
 export default function Sidebar() {
   return (
     <div className="SidebarMenu-wrap">
       <div className="Sidebar">
         <ul>
-          <li>
+          <li><SvgDashboard/>
             <a href=""> Dashboard</a>
           </li>
-          <li>
+          <li><SvgHelp/>
             <a href=""> Pages</a>
           </li>
-          <li>
+          <li><SvgMessages/>
             <a href=""></a>
             Messages
           </li>
-          <li>
+          <li><SvgPages/>
             <a href=""></a>
             Settings
           </li>
-          <li>
+          <li><SvgRoles/>
             <a href=""></a>
             Roles
           </li>
         </ul>
 
         <ul>
-          <li></li>
+          <li><SvgSettings/><a href=""></a>Help</li>
         </ul>
       </div>
     </div>
