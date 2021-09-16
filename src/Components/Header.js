9f3aaddfdfef0eb7664988634f3d62c472075a74
javeriasohrani <83175699+javeriasohrani@users.noreply.github.com>
2021-09-16 02:26:24 -0700

    remove useless code

--- src/Components/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Header() {
-    return (
-        <div>
-            I am header js file
-        </div>
-    )
-}
--- src/Routing/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Routing() {
-    return (
-        <div>
-            g mhg ,jhg ,kj hkj <h1>k .j ,jg ,jh ,jh </h1>
-        </div>
-    )
-}
