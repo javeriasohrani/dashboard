b128501219aacef031f38b685aa862a6151636d8
javeriasohrani <83175699+javeriasohrani@users.noreply.github.com>
2021-09-17 02:53:04 -0700

    update signup and login components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import "./App.css";
-import Header from "./Components/Header";
-import Sidebar from "./Components/Sidebar";
+
 
 
 
 function App() {
   return (
     <div className="App">
-      {/* <Sidebar/> */}
-      <Header/>
+
 
     </div>
   );
